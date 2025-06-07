ef0cf2ac70b7f00e921af937606eccc83fc73c90
Jeremy Kneebone <jezknee@gmail.com>
2025-06-07 17:29:04 +0100

    v0.2.2 read in all the text

---
+++ tests.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+path = Path.cwd()
+
+file_info = []
+texts = []
+for file_path in path.rglob("*"):
+    #if file_path.suffix == ".txt":
+    print(Path(file_path).name)
+    print(file_path.name)
+    file_info.append(Path(file_path).name)
+        #with open(file_path,'r') as fp:
+            #text = fp.readlines()
+            #texts.append(text)
+#print(file_info)
+
+
+
+def read_novels(path=Path.cwd() / "texts" / "novels"):
+    """Reads texts from a directory of .txt files and returns a DataFrame with the text, title,
+    author, and year"""
+    file_info = []
+    texts = []
+    for file_path in path.rglob("*"):
+        print(1)
+        #if file_path.suffix == ".txt":
+        print(Path(file_path).name)
+        print(file_path.name)
+        file_info.append(Path(file_path).name)
+            #with open(file_path,'r') as fp:
+                #text = fp.readlines()
+                #texts.append(text)
+    #print(file_info)
+
+
+read_novels()
